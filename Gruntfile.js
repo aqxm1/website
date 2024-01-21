67e434b9223a228d710d9c8d225141a1d760932a
aqxm1 <adam1612m@web.de>
2024-01-21 13:59:12 +0100

    [feature] Grunt config -install uglify -javascript uglification -watcher config

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,22 @@ module.exports = function(grunt) {
       }
     },
 
+    // Uglify Javascript
+    uglify: {
+      options: {
+        banner: '/! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>/\n'
+      },
+      build: {
+        src: 'src/scripts/**/*.js', //source files to be minified
+        dest: 'src/scripts/dist/<%= pkg.name %>main.min.js' //destination for minified files
+      }
+    },
+
     // Watch for changes and run tasks
     watch: {
       styles: {
-        files: ['src/styles/**/*.scss'],
-        tasks: ['sass', 'cssmin'],
+        files: ['src/styles/**/*.scss','src/scripts/**/*.js'],
+        tasks: ['sass','uglify','cssmin'],
         options: {
           spawn: false,
         },
@@ -40,8 +51,9 @@ module.exports = function(grunt) {
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s)
-  grunt.registerTask('default', ['sass', 'cssmin']);
+  grunt.registerTask('default', ['sass','uglify','cssmin']);
 };
