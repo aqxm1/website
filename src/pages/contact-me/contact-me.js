efb53398cdbdfa4fdb031995d5ddb9cd90363b06
aqxm1 <adam1612m@web.de>
2024-03-29 12:15:07 +0100

    [COMMIT] added img changes

--- src/pages/contact-me/contact-me.js
+++
@@ -1,9 +0,0 @@
-class ContactMe {
-  async render() {
-     const response = await fetch('./pages/contact-me/contact-me.html');
-     const template = await response.text();
-     return template;
-  }
-}
-
-export default new ContactMe();
