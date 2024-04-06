2e40051a5236f663eb0edd3be65b2e97768f350e
aqxm1 <adam1612m@web.de>
2024-04-06 14:25:51 +0200

    [Commit] changes

--- src/assets/scripts/menu/menu.js
+++
@@ -1,9 +0,0 @@
-//document.addEventListener('DOMContentLoaded', function() {
-//  const menuIcon = document.querySelector('.menu-icon');
-//  const menu = document.querySelector('.menu-container');
-//
-//  menuIcon.addEventListener('click', function() {
-//    menu.classList.toggle('show');
-//  });
-//});
-
