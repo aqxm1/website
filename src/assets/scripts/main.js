010ae3285152315b62cc83efae2470886af24571
aqxm1 <adam1612m@web.de>
2024-04-07 19:21:16 +0200

    [commit]
    Changes

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,7 +3,6 @@ import { Router } from './router/router.min.js';
 import Dashboard from '../../pages/dashboard/dashboard.min.js';
 import AboutMe from '../../pages/about-me/about-me.min.js';
 import Lebenslauf from '../../pages/lebenslauf/lebenslauf.min.js';
-import ContactMe from '../../pages/contact-me/contact-me.min.js';
 
 const router = new Router();
 
@@ -20,11 +19,6 @@ router.addRoute({
 router.addRoute({
   path: '/lebenslauf',
   component: Lebenslauf,
-});
-
-router.addRoute({
-  path: '/contact-me',
-  component: ContactMe,
 });
 
 router.start();
